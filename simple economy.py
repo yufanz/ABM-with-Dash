d5e035168982362e4e565fc70029355a19669b73
Yufan Zhang <yufan.adam.zhang@gmail.com>
2019-04-16 20:47:51 -0400

    Cleaned code

--- simple economy.py
+++ simple economy.py
@@ -12,6 +12,20 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+initial_data = [{
+    'x': [10 for i in range(100)],
+    'y': [i for i in range(100)]
+}]
+
+initial_layout = go.Layout(
+    xaxis=dict(
+        range=[-100, 100]
+    ),
+    yaxis=dict(
+        range=[-100, 100]
+    )
+)
+
 app.layout = html.Div([
 
     dcc.Store(
@@ -20,34 +34,38 @@ app.layout = html.Div([
 
     html.Div([
         html.Button('Step', id='step-button'),
-        html.Button('Play', id='flip-button')
+        html.Button('Play / Pause', id='play-button')
     ]),
 
     dcc.Graph(
-        id='graph',
+        id='scatter-plot',
+        figure={
+            'data': [go.Scatter(x=initial_data[0]['x'], y=initial_data[0]['y'], mode='markers')],
+            'layout': initial_layout
+        }
     ),
 
     dcc.Interval(
-        id='interval-component',
+        id='interval',
         interval=1*10,
         n_intervals=0,
         max_intervals=0
     )
 ])
 
-@app.callback(Output('memory', 'data'),
-              [Input('step-button', 'n_clicks'),
-               Input('interval-component', 'n_intervals'),
-               Input('interval-component', 'max_intervals')],
-              [State('memory', 'data')])
+
+@app.callback(
+    Output('memory', 'data'),
+    [Input('step-button', 'n_clicks'),
+     Input('interval', 'n_intervals'),
+     Input('interval', 'max_intervals')],
+    [State('memory', 'data')])
 def step(n_clicks, n_intervals, max_intervals, data):
+
     if n_clicks is None and max_intervals == 0:
         raise PreventUpdate
 
-    data = data or [
-        {'x': [10 for i in range(100)], 
-         'y': [i for i in range(100)]},
-    ]
+    data = data or initial_data
 
     newX = [i - 1 for i in data[0]['x']]
     for i in range(len(newX)):
@@ -56,44 +74,31 @@ def step(n_clicks, n_intervals, max_intervals, data):
 
     return [{'x': newX, 'y': data[0]['y']}]
 
-# output the stored clicks in the table cell.
-@app.callback(Output('graph', 'figure'),
-              [Input('memory', 'modified_timestamp')],
-              [State('memory', 'data')])
-def on_data(ts, data):
-    if ts is None:
-        raise PreventUpdate
-
-    # data = data or {}
-    # return data
-    # print("updating figure")
-
-    return {
-        'data': [
-            go.Scatter(x=data[0]['x'], y=data[0]['y'], mode='markers')
-        ],
-        'layout': go.Layout(
-            xaxis=dict(
-                range=[-100, 100]
-            ),
-            yaxis=dict(
-                range=[-100, 100]
-            )
-        )
-    }
 
 @app.callback(
-    Output('interval-component', 'max_intervals'),
-    [Input('flip-button', 'n_clicks')],
-    [State('interval-component', 'max_intervals')]
-)
-def flip(n_clicks, max_intervals):
+    Output('interval', 'max_intervals'),
+    [Input('play-button', 'n_clicks')],
+    [State('interval', 'max_intervals')])
+def play(n_clicks, max_intervals):
+
     if n_clicks is None:
         raise PreventUpdate
     if max_intervals == -1:
-        return 1
+        return 0
     else:
         return -1
+
+
+@app.callback(
+    Output('scatter-plot', 'figure'),
+    [Input('memory', 'data')])
+def update_figure(data):
+
+    return {
+        'data': [go.Scatter(x=data[0]['x'], y=data[0]['y'], mode='markers')],
+        'layout': initial_layout
+    }
+
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8077, threaded=True)
